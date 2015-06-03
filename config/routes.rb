dd5dce5bd504b44a1650d3f8d22322c84af98d81
Melissa Wallace <mhk33@cornell.edu>
2015-06-03 14:02:03 -0400

    add static collection home pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'ragamala' => 'ragamala#index'
+  get 'reps-bastides' => 'reps_bastides#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
