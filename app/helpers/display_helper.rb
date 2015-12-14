b85753d23c3580240dc36ddeb3b695c70497f5eb
starsplatter <jenncd@gmail.com>
2015-12-14 11:25:02 -0500

    pagination fixes

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -5,12 +5,12 @@ module DisplayHelper
   # Standard display of a SELECTED facet value, no link, special span
   # with class, and 'remove' button.
   def render_selected_facet_value(facet_solr_field, item)
-    #Updated class for Bootstrap Blacklight 
+    #Updated class for Bootstrap Blacklight
     content_tag(:span, render_facet_value(facet_solr_field, item, :suppress_link => true), :class => "selected") +
-      link_to(content_tag(:i, '', :class => "fa fa-times") + content_tag(:span, '[remove]', :class => 'hide-text'), remove_facet_params(facet_solr_field, item, params), :class=>"remove")
+      link_to(content_tag(:i, '', :class => "fa fa-times") + content_tag(:span, '[remove]', :class => 'hide-text'), remove_facet_params(facet_solr_field, item, params.except('sbjct')), :class=>"remove")
   end
 
-  
+
   def presenter_class
     MultilineDisplayPresenterClass
   end
