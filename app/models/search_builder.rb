7fc29750dead5faec2a445b5fccb3a655c98316e
starsplatter <jenncd@gmail.com>
2016-06-06 15:53:00 -0400

    add maps to search builder

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,4 +2,7 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
 
+     self.default_processor_chain += [:add_spatial_search_to_solr]
+
+
 end
