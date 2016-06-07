4ff42bd08976b8daaccdee9f3930f683b2eb6610
Melissa Wallace <mhk33@cornell.edu>
2016-06-07 14:25:15 -0400

    DIGCOLL-616: bring reps slides to prod

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -109,7 +109,8 @@ class CatalogController < ApplicationController
                     OR collection_tesim:"Historic Glacial Images of Alaska and Greenland"
                     OR collection_tesim:"Mysteries at Eleusis: Images of Inscriptions"
                     OR collection_tesim:"Icelandic and Faroese Photographs of Frederick W.W. Howell"
-                    OR collection_tesim:"Alison Mason Kingsbury: Life and Art")'}
+                    OR collection_tesim:"Alison Mason Kingsbury: Life and Art"
+                    OR collection_tesim:"John Reps Collection - Slides")'}
             end
             end
 
