7e046850113f41ceee59513c1acb928bee416b85
Melissa Wallace <mhk33@cornell.edu>
2016-09-30 12:44:53 -0400

    DIGCOLL-752: add active state for context nav pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,11 @@ module ApplicationHelper
       !params[:q].blank? or !params[:f].blank? or !params[:search_field].blank?
     end
 
+    def link_to_in_li(body, url, html_options = {})
+      active = "active" if current_page?(url)
+      content_tag :li, class: active do
+        link_to body, url, html_options
+      end
+    end
+
 end
