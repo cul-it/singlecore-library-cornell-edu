5dd170a2ecd60b71dbd2c8217a54719cba2f2a14
starsplatter <jenncd@gmail.com>
2016-10-19 15:01:23 -0400

    fix label on title sort

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -414,7 +414,7 @@ class CatalogController < ApplicationController
     config.add_sort_field 'score desc, latest_date_isi asc', :label => 'relevance'
     config.add_sort_field 'latest_date_isi desc, title_tesi asc', :label => 'year (descending)'
     config.add_sort_field 'latest_date_isi asc, title_tesi asc', :label => 'year (ascending)'
-    config.add_sort_field 'title_ssi asc, latest_date_isi asc', :label => 'author (a to z)'
+    config.add_sort_field 'title_ssi asc, latest_date_isi asc', :label => 'title (a to z)'
     config.add_sort_field 'author_t asc, latest_date_isi asc', :label => 'author (a to z)'
 
     # If there are more than this many search results, no spelling ("did you
