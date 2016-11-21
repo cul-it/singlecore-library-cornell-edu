2c8c7694467b17eb939d99300269d8c6ca400329
starsplatter <jenncd@gmail.com>
2016-11-21 18:37:27 -0500

    remove deprecated facet method

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -4,11 +4,7 @@ module DisplayHelper
   # -- Replace icon-remove (glyphicon) with appropriate Font Awesome classes: fa and fa-times
   # Standard display of a SELECTED facet value, no link, special span
   # with class, and 'remove' button.
-  def render_selected_facet_value(facet_solr_field, item)
-    #Updated class for Bootstrap Blacklight
-    content_tag(:span, render_facet_value(facet_solr_field, item, :suppress_link => true), :class => "selected") +
-      link_to(content_tag(:i, '', :class => "fa fa-times") + content_tag(:span, '[remove]', :class => 'hide-text'), remove_facet_params(facet_solr_field, item, params.except('sbjct')), :class=>"remove")
-  end
+
 
 def render_collection_title
 	if params[:action] == "show" && render_document_show_field_value(:document => @document, :field => 'collection_tesim').present?
