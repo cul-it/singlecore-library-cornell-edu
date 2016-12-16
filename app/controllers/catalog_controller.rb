e526e14af3c56e287d3ba31ea9c76e9866133589
Melissa Wallace <mhk33@cornell.edu>
2016-12-16 06:20:49 -0500

    DIGCOLL-871: fix label for Location constraint

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -174,6 +174,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'illustrator_creator_tesim', :label => 'Creator', :sort => 'count', :show => false
     config.add_facet_field 'second_creator_tesim', :label => 'Creator', :sort => 'count', :show => false
     config.add_facet_field 'photographer_creator_tesim', :label => 'Photographer', :show => false
+    config.add_facet_field 'location_tesim', :label => 'Location', :show => false
     #config.add_facet_field 'performers_subject_tesim', :label => 'Performers', :show => false
     config.add_facet_field 'type_tesim', :label => 'Work Type', :sort => 'count', :limit => 5
     config.add_facet_field 'system_tesim', :label => 'System', :sort => 'count', :show => false
