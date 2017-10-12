14f0036e754e12e3c77e143a1b01dfad479cc958
Jenn <jenncd@gmail.com>
2017-10-12 10:01:20 -0400

    Dev (#702)
    * Digcoll 834 (#694)
    
    * publication field
    
    * wordsworth collection
    
    * Digcoll 834 (#696)
    
    * publication field
    
    * wordsworth collection
    
    * adjust link styles for hathi
    
    * hathi link styles
    
    * add link to hathi record
    
    * add iwo pdf links (#699)
    
    * Digcoll 1142 (#701)
    
    * add iwo pdf links
    
    * iwo pdf links in index view

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -52,12 +52,13 @@ def image_download options={}
     options[:document] # the original document
     options[:field] # the field to render
     options[:value] # the value of the field
-    if options[:document]['collection_tesim'][0] !='Adler Hip Hop Archive'
-    link_to content_tag(:span, 'Original Image'), options[:value][0],title:"Download",
-    onclick:"javascript:_paq.push(['trackEvent', 'DownloadLink',
-    'viewer-" + options[:document]['project_id_ssi'] + "-" + options[:document]['id'] +"'" + '])'
+    if options[:document]['project_id_ssi'] == "3450"
+      link_to content_tag(:span, 'PDF Document'), options[:document]['media_URL_tesim'][0],title:"Download"
     else
-      return "Not available for download"
+      link_to content_tag(:span, 'Original Image'), options[:value][0],title:"Download",
+      onclick:"javascript:_paq.push(['trackEvent', 'DownloadLink',
+      'viewer-" + options[:document]['project_id_ssi'] + "-" + options[:document]['id'] +"'" + '])'
+
     end
 
   end
