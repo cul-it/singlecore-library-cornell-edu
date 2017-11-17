48cd0021034ed62561235e1c8f85bd1903fc09ce
Melissa Wallace <mhk33@cornell.edu>
2017-11-17 11:25:53 -0500

    clean up that test

--- spec/features/download_spec.rb
+++ spec/features/download_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 require 'rails_helper'
-# Feature: Home page
-#   As a visitor
-#   I want to visit a home page
-#   So I can learn more about the website
 RSpec.feature 'Item download' do
 
-  # Scenario: Visit the home page
-  #   Given I am a visitor
-  #   When I visit the home page
-  #   Then I see "Welcome"
   scenario 'visit a Persuasive Maps Ernst Adler item view' do
     visit solr_document_path('ss:19343284')
     expect(body).not_to have_content 'Download'
