dfda8bcbfb73c92c6bb2151dcc0cad7dd489b348
Melissa Wallace <mhk33@cornell.edu>
2018-05-29 11:26:10 -0400

    Update tests

--- spec/features/download_spec.rb
+++ spec/features/download_spec.rb
@@ -17,13 +17,8 @@ RSpec.feature 'Item download' do
     expect(body).to have_content 'Download'
   end
 
-  scenario 'Paniccioli should not have download on item view' do
+  scenario 'A Paniccioli should not have download on item view' do
     visit solr_document_path('ss:22741489')
     expect(body).not_to have_content 'Download'
-  end
-
-  scenario 'Download PDF link on item view' do
-    visit solr_document_path('ss:21072993')
-    expect(body).not_to have_content 'Download item'
   end
 end
