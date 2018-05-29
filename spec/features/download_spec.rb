6dfcc364ed957982d269034be69375364cdf1e5b
Melissa Wallace <mhk33@cornell.edu>
2018-05-29 11:16:21 -0400

    DIGCOLL-943: disable downloads using disable download field; add download tests

--- spec/features/download_spec.rb
+++ spec/features/download_spec.rb
@@ -12,8 +12,18 @@ RSpec.feature 'Item download' do
     expect(body).to have_content 'Download'
   end
 
-  scenario 'visit an item view without creator' do
+  scenario 'visit another item that should have download' do
     visit solr_document_path('ss:3875086')
     expect(body).to have_content 'Download'
+  end
+
+  scenario 'Paniccioli should not have download on item view' do
+    visit solr_document_path('ss:22741489')
+    expect(body).not_to have_content 'Download'
+  end
+
+  scenario 'Download PDF link on item view' do
+    visit solr_document_path('ss:21072993')
+    expect(body).not_to have_content 'Download item'
   end
 end
