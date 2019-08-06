7d0805d779b0486e694eef94dac810407e3297c7
jac244 <jac244@cornell.edu>
2019-08-06 15:47:39 -0400

    update for may collection

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -4,6 +4,10 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   include BlacklightRangeLimit::RangeLimitBuilder
 
-
+  self.default_processor_chain += [:full_text_search]
+    
+  def full_text_search user_parameters
+   
+  end
 
 end
