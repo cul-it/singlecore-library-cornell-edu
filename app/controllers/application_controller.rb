512f89d56281972cb76befca755612aea195f99d
Melissa Wallace <mhk33@cornell.edu>
2019-08-13 15:22:55 -0400

    May antislavery collection not yet ready for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < ActionController::Base
       fq = '(collection_tesim:"Adler Hip Hop Archive"  AND -adler_status:"Suppress for portal")
       OR display_target_tesim:"bento"
       OR collection_tesim:"Indonesian Music Archive"
+      OR -collection_tesim:"Samuel J. May Anti-Slavery Collection"
       OR -format_tesim:"Page"
       OR (-status_ssi:"Unpublished" AND -status_ssi:"Suppressed" AND -active_fedora_model_ssi:"Page" AND -solr_loader_tesim:"eCommons"
       AND +(collection_tesim:"New York State Aerial Photographs"
