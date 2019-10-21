0dfa1f26d6af20ea9424d6d8d756456bd911d46f
James Reidy <jreidy@cornell.edu>
2019-10-21 11:38:39 -0400

    get rid of DEPRECIATION WARNING   discard_flash_if_xhr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+  skip_after_action :discard_flash_if_xhr
   layout 'blacklight'
 
   def set_fq(environment)
