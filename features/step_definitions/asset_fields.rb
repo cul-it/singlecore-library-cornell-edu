9346c454be2461c4e365a59e28bcc7d4de7ce193
James Reidy <jreidy@cornell.edu>
2019-10-22 10:54:01 -0400

    steps to navigate to asset, check labeled fields in detail view

--- features/step_definitions/asset_fields.rb
+++ features/step_definitions/asset_fields.rb
@@ -9,3 +9,20 @@ Then("the page header should contain {string}") do |string|
         expect(page.find(:css, 'h1')).to have_content "#{string}"
     end
 end
+
+Given("I go to asset id {string}") do |string|
+    visit("/catalog/ss:#{string}")
+end
+
+Then("I should see the field labeled {string}") do |string|
+    within "div.item-info" do
+        find(:xpath, "//dt[text()='#{string}:']")
+    end
+end
+
+
+Then("the field labeled {string} should begin with {string}") do |string, string2|
+    within "div.item-info" do
+        find(:xpath, "//dt[text()='#{string}:']").first(:xpath, "//following-sibling::dd[starts-with(.,'#{string2}')]")
+    end
+end
