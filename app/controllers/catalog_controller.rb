648543f989a961c0d1433d26116036cbf84d8c95
James Reidy <jreidy@cornell.edu>
2019-10-28 09:30:17 -0400

    show just the first date field on the index (search results) page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -326,7 +326,7 @@ class CatalogController < ApplicationController
 
     config.add_index_field 'image_ocr_tesim',:label => 'text'
     config.add_index_field 'book_title', :label => 'Book Title'
-    config.add_index_field 'date_tesim', :label => 'Date'
+    config.add_index_field 'r1_date_tesim', :label => 'Date'
     config.add_index_field 'serial_pub_date_range_ssi', :label => 'Publication Date Range'
 
     #config.add_index_field 'download_link_tesim', helper_method: 'image_download', :label => 'Download'
