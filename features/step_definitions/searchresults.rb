dc7beb045d8a20817fd8f932d1b0a81e92c479f4
James Reidy <jreidy@cornell.edu>
2019-10-29 08:08:55 -0400

    cucumber step to verify fields on first search result

--- features/step_definitions/searchresults.rb
+++ features/step_definitions/searchresults.rb
@@ -11,3 +11,11 @@ Then("I search for asset {string}") do |string|
   fill_in('q', :with => string)
   page.find(:css, 'button#search').click
 end
+
+Then("the first search result should have field {string} starting {string}") do |string, string2|
+  within ('div#documents') do
+    within ('div.document-position-0 dl') do
+      find(:xpath, "//dt[text()='#{string}:']", match: :first).first(:xpath, "//following-sibling::dd[starts-with(.,'#{string2}')]")
+    end
+  end
+end
