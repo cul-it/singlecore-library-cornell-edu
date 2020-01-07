851f5a0597c3dde65b7ddf1373c0d17c9af3b083
Melissa Wallace <mhk33@cornell.edu>
2020-01-07 10:56:01 -0500

    DIGCOLL-1522: add thumbnail to results for awsthumbnail_tesim

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,24 +6,27 @@ module ApplicationHelper
     fedora_type = render_index_field_value :document => document, :field => "active_fedora_model_ssi"
     ss_thumb = render_index_field_value :document => document, :field => "media_URL_size_2_tesim"
     dlxs_thumb = "http://hydrastg.library.cornell.edu/fedora/objects/#{document[:id]}/datastreams/thumbnailImage/content"
+    aws_thumb = render_index_field_value :document => document, :field => "awsthumbnail_tesim"
     if ss_thumb.present?
-      return  image_tag(ss_thumb, options={})
+      return image_tag(ss_thumb, options={})
+    end
+    if aws_thumb.present?
+      return image_tag(aws_thumb, options={})
     end
     if fedora_type.present?
       image_tag(dlxs_thumb, options={})
     end
   end
 
+  def has_search_parameters?
+    !params[:q].blank? or !params[:f].blank? or !params[:search_field].blank?
+  end
 
-    def has_search_parameters?
-      !params[:q].blank? or !params[:f].blank? or !params[:search_field].blank?
+  def link_to_in_li(body, url, html_options = {})
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url, html_options
     end
-
-    def link_to_in_li(body, url, html_options = {})
-      active = "active" if current_page?(url)
-      content_tag :li, class: active do
-        link_to body, url, html_options
-      end
-    end
+  end
 
 end
