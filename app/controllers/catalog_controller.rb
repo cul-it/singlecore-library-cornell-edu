1b17f1e87db3993c6919c29c602acdcc09667a14
Melissa Wallace <mhk33@cornell.edu>
2020-02-11 09:25:22 -0500

    Do not display image ocr text field in results. For now.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -320,7 +320,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'pubplace_tesim', :label => 'Publication Place'
     config.add_index_field 'pubdate_tesim', :label => 'Date'
 
-    config.add_index_field 'image_ocr_tesim',:label => 'text'
+    # config.add_index_field 'image_ocr_tesim',:label => 'text'
     config.add_index_field 'book_title', :label => 'Book Title'
     config.add_index_field 'date_tesim', :label => 'Date'
     config.add_index_field 'serial_pub_date_range_ssi', :label => 'Publication Date Range'
