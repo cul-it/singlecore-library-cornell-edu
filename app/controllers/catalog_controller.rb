2e5546c99b0b8875e0949e48c430f189f083b28d
James Reidy <jreidy@cornell.edu>
2020-04-15 14:51:02 -0400

    Turn Yiddish (Romanized) into Title (Yiddish Romanized)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -541,7 +541,8 @@ class CatalogController < ApplicationController
     role = parts.first + '_title_language_' + parts.last
     qualifier = solr_doc[role]
     if qualifier.present?
-      field_config['label'] = 'Title (' + qualifier.first.split.map(&:capitalize).join(' ') + ')'
+      // remove parens then prefix and surround with parens
+      field_config['label'] = 'Title (' + qualifier.first.gsub(/[()]/,"").split.map(&:capitalize).join(' ') + ')'
     end
     return true
   end
