fb50f89663b87be38e6e54e3a4b35ef129186a60
James Reidy <jreidy@cornell.edu>
2020-06-18 15:16:35 -0400

    correct collection logic

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -423,7 +423,7 @@ def autolink_field args
   Rails.logger.info("MIAMI = #{args}")
   if !args[:document]["collection_tesim"].nil?
    collection = args[:document]["collection_tesim"][0]
-   if collection == "Persuasive Maps: PJ Mode Collection" || "Digitizing Tell en-Naṣbeh, Biblical Mizpah of Benjamin"
+   if (collection == "Persuasive Maps: PJ Mode Collection" || collection == "Digitizing Tell en-Naṣbeh, Biblical Mizpah of Benjamin")
     return auto_link(args[:document][args[:field]].join("<br>")).html_safe
    else
     return args[:document][args[:field]].join("<br>").html_safe
