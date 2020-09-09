ccaefee5ac3b28ef8a4a0cff1a7453c262cbafdd
Melissa Wallace <mhk33@cornell.edu>
2020-09-09 11:15:08 -0400

    DIGCOLL-1708: kmoddl landing page edits; update voigt number label

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -453,7 +453,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'old_catalog_number_tesim', :label => 'Old Catalog Number'
     config.add_show_field 'bam_number_tesim', :label => 'Bam Number'
     config.add_show_field 'item_number_tesim', :label => 'Item Number'
-    config.add_show_field 'voight_number_tesim', :label => 'Voigt Catalog Model Number'
+    config.add_show_field 'voight_number_tesim', :label => 'Voigt Catalog Number'
 
     # collection- and item-specific
     config.add_show_field 'occasion_tesim', :label => 'Occasion'
