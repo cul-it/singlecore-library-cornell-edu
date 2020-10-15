618915a9461ad83e0081943cc807ae9473b1077c
James Reidy <jreidy@cornell.edu>
2020-10-15 15:21:50 -0400

    DIGCOLL-1585 - location_tesim is now location_facet_tesim

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -328,7 +328,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'volume_tesim', :label => 'Volume'
     config.add_index_field 'page_tesim', :label => 'Page'
     config.add_index_field 'creation_site_location_tesim', :label => 'Site', :link_to_search => true
-    config.add_index_field 'location_tesim', :label => 'Location', :link_to_search => true
+    config.add_index_field 'location_facet_tesim', :label => 'Location', :link_to_search => true
 
     # these index fields are from the dlxs collections
     config.add_index_field 'publication_tesim', :label => 'Publication'
