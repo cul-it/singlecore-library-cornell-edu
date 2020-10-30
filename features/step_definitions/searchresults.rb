44a824b83e914d60d678f4e501845b16744491da
James Reidy <jreidy@cornell.edu>
2020-10-30 09:42:28 -0400

    DIGCOLL-1797 - work in progress for sorting results

--- features/step_definitions/searchresults.rb
+++ features/step_definitions/searchresults.rb
@@ -95,3 +95,56 @@ Then("I should not see id {string} in the search results") do |string|
     expect(row).not_to have_selector(link)
   end
 end
+
+def brute
+  visit('/?f[collection_tesim][]=19th+Century+Prison+Reform+Collection&sort=latest_date_isi+desc%2C+title_tesi+asc')
+end
+
+def refresh_current_page
+  uri = URI(page.current_url.to_s)
+  path = uri.path
+  path += ('?' + uri.query) if uri.query.present?
+  path += ('#' + uri.fragment) if uri.fragment.present?
+#******************
+save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+Rails.logger.warn "jgr25_log\n#{__method__} #{__LINE__} #{__FILE__}:"
+msg = [" #{__method__} ".center(60,'Z')]
+msg << "path: " + path.inspect
+msg << 'Z' * 60
+puts msg.to_yaml
+Rails.logger.level = save_level
+#*******************
+  visit('/')
+  visit(path)
+end
+
+Given("I sort the results by {string}") do |string|
+  # within page.find('div#sort-dropdown') do
+  #   # find('ul.dropdown-menu > li', :text => string).click
+  #   find('ul.dropdown-menu').select(string)
+  # end
+  # page.find('div#sort-dropdown').select(string)
+  menu = page.find('div#sort-dropdown')
+  # open the dropdown to reveal the choices
+  menu.click
+  # what_is menu
+  # choice = page.find('div#sort-dropdown.open > ul > li > a', text: "#{string}")
+  # what_is choice
+  # choice.trigger('click')
+  # changes page.current_url but does not actually go to the page
+  href = page.find('div#sort-dropdown.open > ul > li > a', text: "#{string}")[:href]
+  puts 'href: ' + URI.unescape(href)
+  page.find('div#sort-dropdown.open > ul.dropdown-menu').click_link("#{string}", :visible => :all)
+  # refresh_current_page
+  brute
+
+  sleep 5
+  header = page.find('div#documents > div.document-position-0 > div.documentHeader')
+  what_is header
+ end
+
+Then("the first search result title should start with {string}") do |string|
+  within page.find('div#documents > div.document-position-0') do
+    expect(find('div.documentHeader')).to have_content("#{string}")
+  end
+end
