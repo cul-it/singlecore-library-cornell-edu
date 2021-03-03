e59326405f2b09159a61750ed1852caded1fab01
James Reidy <jreidy@cornell.edu>
2021-03-03 17:33:35 -0500

    store and sort collection information

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,9 +1,22 @@
 class CollectionsController < ApplicationController
   include CollectionsHelper
+  Collection = Struct.new(:nickname, :title, :image, :landing, :search)
 
   def index
     nicknames = get_collection_nicknames()
-    @examples = []
-    nicknames.each { |n| @examples << get_collection_example(n) }
+    @collections = []
+    nicknames.each { |n|
+      item = get_collection_example(n)
+      @collections << Collection.new(
+        n,
+        item["collection_tesim"][0],
+        get_image(n),
+        get_landing_page(n),
+        "/cq/#{n}"
+      )
+    }
+    @collections = @collections.sort_by { |collection| [collection.title] }
   end
+
 end
+
